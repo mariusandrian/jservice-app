8ccf7ed31b25456072ac0e50203c19c6b5ffd8bc
Marius Andrian <marius.atj@gmail.com>
2020-07-20 23:27:03 +0800

    Updates

---
+++ precache-manifest.c5b87ac3600d0e2a24c0ff0f031349cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6620075da9e7fe65e24b7ee3072572",
+    "url": "/jservice-app/index.html"
+  },
+  {
+    "revision": "6c9e01c6c40a963f9ff7",
+    "url": "/jservice-app/static/css/main.e97f0f83.chunk.css"
+  },
+  {
+    "revision": "a11dc09d9309b79267dc",
+    "url": "/jservice-app/static/js/2.cd9f0d3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jservice-app/static/js/2.cd9f0d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9e01c6c40a963f9ff7",
+    "url": "/jservice-app/static/js/main.82ba2f77.chunk.js"
+  },
+  {
+    "revision": "1b3c4e1e46674a0ee5b1",
+    "url": "/jservice-app/static/js/runtime-main.958a0d25.js"
+  }
+]);
